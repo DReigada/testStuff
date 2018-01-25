ca1c756365905d4aa8e18d5d4ef24d4d3427d318
Daniel Reigada <daniel.reigada@gmail.com>
2018-01-25 11:33:10 +0000

    Add languages file

---
+++ caseObj.scala
@@ -0,0 +1,105 @@
+sealed trait Language extends EnumEntry
+
+object Language {
+
+  case object Javascript extends Language
+
+  case object Scala extends Language
+
+  case object CSS extends Language
+
+  case object PHP extends Language
+
+  case object Python extends Language
+
+  case object Ruby extends Language
+
+  case object Java extends Language
+
+  case object CoffeeScript extends Language
+
+  case object Swift extends Language
+
+  case object CPP extends Language
+
+  case object C extends Language
+
+  case object Shell extends Language
+
+  case object TypeScript extends Language
+
+  case object Dockerfile extends Language
+
+  case object SQL extends Language
+
+  case object PLSQL extends Language
+
+  case object JSON extends Language
+
+  case object SASS extends Language
+
+  case object LESS extends Language
+
+  case object Go extends Language
+
+  case object JSP extends Language
+
+  case object Velocity extends Language
+
+  case object XML extends Language
+
+  case object Apex extends Language
+
+  case object Elixir extends Language
+
+  case object Clojure extends Language
+
+  case object Rust extends Language
+
+  case object Haskell extends Language
+
+  case object Erlang extends Language
+
+  case object YAML extends Language
+
+  case object Dart extends Language
+
+  case object Elm extends Language
+
+  case object HTML extends Language
+
+  case object Groovy extends Language
+
+  case object VisualForce extends Language
+
+  case object Perl extends Language
+
+  case object CSharp extends Language
+
+  case object VisualBasic extends Language
+
+  case object ObjectiveC extends Language
+
+  case object FSharp extends Language
+
+  case object Cobol extends Language
+
+  case object Fortran extends Language
+
+  case object R extends Language
+
+  case object Scratch extends Language
+
+  case object Lua extends Language
+
+  case object Lisp extends Language
+
+  case object Prolog extends Language
+
+  case object Julia extends Language
+
+  case object Kotlin extends Language
+
+  case object OCaml extends Language
+
+}
