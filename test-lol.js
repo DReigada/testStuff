f32fa560f465e8d1abb5966d1c67a9220f2e43bb
Daniel Reigada <daniel@codacy.com>
2019-11-06 17:42:00 +0000

    Some test

---
+++ test-lol.js
@@ -0,0 +1,4 @@
+import 'a-fourth-dependency'; // none
+import * from 'some-dependancy'; // all
+import ThisClass from 'some-other-dependency'; // single
+import { aMethod, bMethod } from 'a-third-dependency'; // multiple
