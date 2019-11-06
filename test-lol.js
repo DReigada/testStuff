6a467af802e7e84fd796a0acc5f272308be07a41
Daniel Reigada <daniel@codacy.com>
2019-11-06 17:48:00 +0000

    Update test-lol.js

--- test-lol.js
+++ test-lol.js
@@ -1,4 +1,4 @@
-import 'a-fourth-dependency'; // none
-import * from 'some-dependancy'; // all
-import ThisClass from 'some-other-dependency'; // single
-import { aMethod, bMethod } from 'a-third-dependency'; // multiple
+import 'a-fourth-dependencya'; // none
+import * from 'some-dependancya'; // all
+import ThisClass from 'some-other-dependencya'; // single
+import { aMethod, bMethod } from 'a-third-dependencya'; // multiple
