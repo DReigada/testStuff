08a79836cb589aec083b14d6f951e27b3ec12ac0
Daniel Reigada <daniel@codacy.com>
2020-01-06 16:29:09 +0000

    Update test-lol.js

--- test-lol.js
+++ test-lol.js
@@ -2,3 +2,5 @@ import 'a-fourth-dependencya'; // none
 import * from 'some-dependancya'; // all
 import ThisClass from 'some-other-dependencya'; // single
 import { aMethod, bMethod } from 'a-third-dependencya'; // multiple
+
+var emailExpression = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
