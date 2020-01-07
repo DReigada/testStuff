d669fd2b9493eb5b81394a16d7c5be6159e31dec
Daniel Reigada <daniel@codacy.com>
2020-01-07 18:45:21 +0000

    Update test.js

--- test.js
+++ test.js
@@ -1,2 +1,4 @@
 var t = {"\b",1};
 var emailExpression = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+var emailExpression2 = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+var emailExpression3 = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
