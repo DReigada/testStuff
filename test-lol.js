cb9dff1bd2d74d6e70d7ac7165aacc0ce347194f
Daniel Reigada <daniel.reigada@gmail.com>
2020-01-30 14:34:07 +0000

    Delete js json files

--- test-lol.js
+++
@@ -1,6 +0,0 @@
-import 'a-fourth-dependencya'; // none
-import * from 'some-dependancya'; // all
-import ThisClass from 'some-other-dependencya'; // single
-import { aMethod, bMethod } from 'a-third-dependencya'; // multiple
-
-var emailExpression = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
